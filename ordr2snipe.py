e9990d6cfb1ffd95dadbabe2d20a97b7025cfcd9
Evi Vanoost <vanooste@rcbi.rochester.edu>
2023-10-23 09:13:44 -0400

    Don't search for names in ORDR, they're not reliable enough

--- ordr2snipe.py
+++ ordr2snipe.py
@@ -38,8 +38,6 @@ parameter = os.getenv('ORDR_PARAM', None)
 value = os.getenv('ORDR_VALUE', None)
 query_param = ""
 
-invalid_names = ["LGWEBOSID"]
-
 if parameter in valid_params:
     query_param = f"?{parameter}={value}"
 
@@ -130,12 +128,7 @@ while next_page:
         else:
             asset_tag = "ordr-" + name
 
-        if name in invalid_names:
-            search_name = ""
-        else:
-            search_name = name
-
-        snipe_asset = get_snipe_asset(serial=serial, mac_address=macaddress, name=search_name, asset_tag=asset_tag)
+        snipe_asset = get_snipe_asset(serial=serial, mac_address=macaddress, asset_tag=asset_tag)
 
         if snipe_asset['total'] > 1:
             logging.error(f"Multiple assets in Snipe-IT for {name}")
